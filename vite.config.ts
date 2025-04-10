7a597b1c6fb0c6a311cb006c9e0e6ab7674efc03
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-04-10 23:30:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: "/arjun-sai-portfolio/", // 👈 important for GitHub Pages
+  plugins: [react()],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
